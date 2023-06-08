39b898e96b65850e58b459f4d5ef5d98131e3fd4
Belle Hollander <bellehollander@gmail.com>
2023-06-08 12:41:53 -0400

    you can login

---
+++ hypo-help/src/components/nav/NavBar.js
@@ -0,0 +1,32 @@
+import { Link, useNavigate } from "react-router-dom";
+import "./NavBar.css";
+
+export const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ul className="navbar">
+      <li className="navbar__item active">
+        <Link className="navbar__link" to="/viewchat">
+          View Chat
+        </Link>
+      </li>
+      {localStorage.getItem("hypo_user") ? (
+        <li className="navbar__item navbar__logout">
+          <Link
+            className="navbar__link"
+            to=""
+            onClick={() => {
+              localStorage.removeItem("hypo_user");
+              navigate("/", { replace: true });
+            }}
+          >
+            Logout
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}
+    </ul>
+  );
+};
---
+++ hypo-help/src/components/views/ApplicationViews.js
@@ -0,0 +1,11 @@
+export const ApplicationViews = () => {
+  return (
+    <>
+      <h1 className="title--main">HYPO-HELP</h1>
+      <div>
+        Helping those who struggle with health anxiety, one less scary google
+        search at a time
+      </div>
+    </>
+  );
+};
